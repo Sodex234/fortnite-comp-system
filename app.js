c2cc22ef4886fe5b2f870f8232523e5a0d44f2be
sodex234 <sodex.gaming@gmail.com>
2018-06-12 15:55:24 +0000

    Update

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const expressLayouts = require('express-ejs-layouts');
+const path = require("path");
+
 var app = express();
 
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.listen(process.env.PORT || 3000);
