89a149eaac38c825eb14a819adec45525f1a34e6
sodex234 <sodex.gaming@gmail.com>
2018-06-28 21:25:52 +0100

    Created a design that doesn't look bad.

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.use("/", require("./routes/index"));
+app.use("/fortnite", require("./routes/fortnite"));
+
 app.listen(process.env.PORT || 3000);
